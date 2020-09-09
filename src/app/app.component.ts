fcb47a0222e64ea47c0b4dea187a0ef71dc20bd5
manish <manishpamnani169@gmail.com>
2020-09-09 13:19:04 +0530

    Responsiveness: Checkpoint# 2;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,4 +15,12 @@ export class AppComponent {
     console.log(el)
     el.scrollIntoView()
   }
+
+  showMenu() {
+    let menu = document.getElementById('menu');
+    menu.style.display = 'block'
+    menu.style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5))';
+    let name = document.getElementsByClassName('name')[0];
+    name['style']['display'] = 'none'
+  }
 }
